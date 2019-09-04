ca2f707cc610568ca4529c9a3d65fdde0936fcd2
Christoph Hoffmann <christoph.hoffmann@uni-greifswald.de>
2019-09-04 13:49:55 +0200

    added versioneer

--- __init__.py
+++ __init__.py
@@ -6,3 +6,6 @@ Created on Fri Jul 19 14:04:03 2019
 """
 
  
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -6,8 +6,12 @@ Created on Fri Jul 19 14:04:59 2019
 """
 
 from setuptools import setup
+import versioneer
 
 packages=['mjoindex_omi','mjoindex_omi.tests']
 
 setup(name='mjoindex_omi',
-      packages=packages, install_requires=['numpy', 'pandas', 'pytest', 'scipy'])
+      packages=packages,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      install_requires=['numpy', 'pandas', 'pytest', 'scipy'])
