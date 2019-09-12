7947aac01823799dacee81f7af3f9acda0476272
Christoph Hoffmann <christoph.hoffmann@uni-greifswald.de>
2019-09-12 15:23:31 +0200

    Bugfixes

--- src/mjoindices/_version.py
+++ src/mjoindices/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = "mjoindices-"
     cfg.parentdir_prefix = "mjoindices-"
-    cfg.versionfile_source = "./mjoindices/_version.py"
+    cfg.versionfile_source = "./src/mjoindices/_version.py"
     cfg.verbose = False
     return cfg
 
