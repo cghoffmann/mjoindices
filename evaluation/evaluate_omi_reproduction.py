5bc65c44d291534b878c4324cf8eb551d741f936
Christoph Hoffmann <christoph.hoffmann@uni-greifswald.de>
2019-09-27 12:55:58 +0200

    Proceeded in implementing evaluation routines and plots -  finished eval of PCs and EOFs.
    Still missing:
    -Explained variance
    -Legends in plots etc
    -Saving of figs

--- evaluation/evaluate_omi_reproduction.py
+++ evaluation/evaluate_omi_reproduction.py
@@ -55,104 +55,31 @@ eofnpzfile = Path(__file__).parents[1] / "examples" / "example_data" / "EOFs.npz
 # The PCs
 pctxtfile = Path(__file__).parents[1] / "examples" / "example_data" / "PCs.txt"
 
-### evaluate EOFs
+########### evaluate EOFs
 
 eofs = eof.restore_all_eofs_from_npzfile(eofnpzfile)
 orig_eofs = eof.load_all_original_eofs_from_directory(originalOMIDataDirname)
+mjoindices.evaluation_tools.plot_comparison_stats_for_eofs_all_doys(eofs, orig_eofs, exclude_doy366=True,do_print=True)
 
-# Check correlation with original EOFs
-fig = eof.plot_correlation_with_original_eofs(eofs, orig_eofs)
-fig.savefig("EOFs_CorrelationWithOriginal.png")
-corr1, corr2 = mjoindices.evaluation_tools.calc_correlations_of_eofs_all_doys(orig_eofs, eofs)
-print(corr1.shape)
-#leave out DOY 366 since this is intended to be different
-corr1 = corr1[0:-1]
-corr2 = corr2[0:-1]
-print(corr1.shape)
-print("Minimum Correlation of EOF1: %f" % np.min(corr1))
-print("Minimum Correlation of EOF2: %f" % np.min(corr2))
+doy=21 #among the best agreements
+#doy=218 #worst agreement
+mjoindices.evaluation_tools.plot_vector_agreement(orig_eofs.eof1vector_for_doy(doy), eofs.eof1vector_for_doy(doy),title="EOF1 for DOY %i"%doy,do_print=True)
+eof.plot_individual_eof_map_comparison(orig_eofs.eofdata_for_doy(doy), eofs.eofdata_for_doy(doy),doy=doy)
 
-
-mjoindices.evaluation_tools.plot_maxdifference_of_eofs_all_doys(orig_eofs,eofs)
-
-#make respective plots
-maxdiff_abs_1, maxdiff_abs_2, maxdiff_rel_1, maxdiff_rel_2 = mjoindices.evaluation_tools.calc_maxdifference_of_eofs_all_doys(orig_eofs, eofs)
-maxdiff_abs_1 = maxdiff_abs_1[0:-1]
-maxdiff_abs_2 = maxdiff_abs_2[0:-1]
-maxdiff_rel_1 = maxdiff_rel_1[0:-1]
-maxdiff_rel_2 = maxdiff_rel_2[0:-1]
-
-print("Maximum rel. difference EOFs1: %f" % np.median(maxdiff_rel_1))
-print("Maximum rel. difference EOFs2: %f" % np.median(maxdiff_rel_2))
-print("Maximum abs. difference EOFs1: %f" % np.median(maxdiff_abs_1))
-print("Maximum abs. difference EOFs2: %f" % np.median(maxdiff_abs_2))
-
-#calc also rel. mean difference
-
-#median difference
-
+########### Evaluate Explained Variance
 #relative deviations of explained variance
 
-##### PCs
-
-#correlations
-#max deviation abs and rel
-#mean deviation
-
-#### correlation and rel differences of strengths
+########### evalute PCs
 
 pcs = pc.load_pcs_from_txt_file(pctxtfile)
-strength = np.square(pcs.pc1) + np.square(pcs.pc2)
 orig_pcs = pc.load_original_pcs_from_txt_file(originalOMIPCFile)
-orig_strength = np.square(orig_pcs.pc1) + np.square(orig_pcs.pc2)
-print(np.corrcoef(orig_strength, strength)[0, 1])
 
 mjoindices.evaluation_tools.plot_timeseries_agreement(orig_pcs.pc1, orig_pcs.time, pcs.pc1, pcs.time, title="PC1", do_print=True)
+mjoindices.evaluation_tools.plot_timeseries_agreement(orig_pcs.pc2, orig_pcs.time, pcs.pc2, pcs.time, title="PC2", do_print=True)
 
+strength = np.square(pcs.pc1) + np.square(pcs.pc2)
+orig_strength = np.square(orig_pcs.pc1) + np.square(orig_pcs.pc2)
 mjoindices.evaluation_tools.plot_timeseries_agreement(orig_strength, orig_pcs.time, strength, pcs.time, title="MJO Strength", do_print=True)
 
 
 
-# fig, axs = plt.subplots(2, 1, num="ReproduceOriginalOMIPCs_PCs", clear=True, figsize=(8, 6), dpi=150)
-# plt.subplots_adjust(hspace=0.3)
-# fig.suptitle("Strength Recalculation")
-# ax = axs[0]
-# ax.set_title("Stength")
-# p1, = ax.plot(orig_pcs.time, orig_strength, label="Original")
-# p2, = ax.plot(pcs.time, strength, label="Recalculation")
-#
-# doys = tools.calc_day_of_year(orig_pcs.time)
-# doy_not_366_inds = np.nonzero(doys != 366)
-#
-# error_quant = (orig_strength[doy_not_366_inds]-strength[doy_not_366_inds])/np.mean(orig_strength[doy_not_366_inds])*100
-#
-# plt.hist(error_quant, bins=100)
-# print(np.mean(error_quant))
-# print(np.std(error_quant))
-# print(np.max(np.abs(error_quant)))
-#
-# fig, ax = plt.subplots(1, 1, num="ReproduceOriginalOMIPCs_PCsxx", clear=True, figsize=(8, 6), dpi=150)
-# #ax = axs[0]
-# plt.plot(orig_strength, strength, linestyle='None', marker="x")
-#
-# doys = tools.calc_day_of_year(orig_pcs.time)
-# doy366_inds = np.nonzero(doys == 366)
-# print(np.array(doy366_inds)/365)
-# plt.plot(orig_strength[doy366_inds], strength[doy366_inds], linestyle='None', marker="x", color="red")
-
-# # Check the explained variance by the EOFS. Values are lower than in Kiladis, 2014, which is correct!
-# fig = eof.plot_explained_variance_for_all_doys(eofs)
-# fig.savefig("EOFs_ExplainedVariance.png")
-#
-# # Check details of the EOF pair for a particular doy in the following
-# doy=50
-# # Plot EOFs for this DOY
-# fig = eof.plot_individual_eof_map(eofs.eofdata_for_doy(doy), doy)
-# fig.savefig("EOF_Sample.png")
-# # Plot EOF pair in comparison to the original one fpr this DOY
-# fig = eof.plot_individual_eof_map_comparison(orig_eofs.eofdata_for_doy(doy), eofs.eofdata_for_doy(doy), doy)
-# fig.savefig("EOF_SampleComparison.png")
-# # Plot the explained variance for the first 10 EOFs of this DOY to check to drop of explained variance after EOF2
-# fig = eof.plot_individual_explained_variance_all_eofs(eofs.eofdata_for_doy(doy), doy=doy, max_eof_number=10)
-# fig.savefig("EOF_SampleExplainedVariance.png")
-#
--- src/mjoindices/evaluation_tools.py
+++ src/mjoindices/evaluation_tools.py
@@ -30,57 +30,8 @@ from scipy.stats import norm
 import mjoindices.empirical_orthogonal_functions as eof
 import mjoindices.tools as tools
 
-
-
-def calc_correlations_of_eofs_all_doys(eofs1: eof.EOFDataForAllDOYs, eofs2: eof.EOFDataForAllDOYs) -> typing.Tuple:
-    doys = eof.doy_list()
-    corr1 = np.empty(doys.size)
-    corr2 = np.empty(doys.size)
-    for idx, doy in enumerate(doys):
-        corr1[idx] = \
-            (np.corrcoef(eofs1.eofdata_for_doy(doy).eof1vector, eofs2.eofdata_for_doy(doy).eof1vector))[0, 1]
-        corr2[idx] = \
-            (np.corrcoef(eofs1.eofdata_for_doy(doy).eof2vector, eofs2.eofdata_for_doy(doy).eof2vector))[0, 1]
-    return corr1, corr2
-
-
-
-def calc_maxdifference_of_eofs_all_doys(eofs_ref: eof.EOFDataForAllDOYs, eofs: eof.EOFDataForAllDOYs) -> typing.Tuple:
-    doys = eof.doy_list()
-    maxdiff_abs_1 = np.empty(doys.size)
-    maxdiff_abs_2 = np.empty(doys.size)
-    maxdiff_rel_1 = np.empty(doys.size)
-    maxdiff_rel_2 = np.empty(doys.size)
-    for idx, doy in enumerate(doys):
-        maxdiff_abs_1[idx] = \
-            np.max(np.abs(eofs_ref.eofdata_for_doy(doy).eof1vector - eofs.eofdata_for_doy(doy).eof1vector))
-        maxdiff_abs_2[idx] = \
-            np.max(np.abs(eofs_ref.eofdata_for_doy(doy).eof2vector - eofs.eofdata_for_doy(doy).eof2vector))
-        maxdiff_rel_1[idx] = maxdiff_abs_1[idx] / np.mean(np.abs(eofs_ref.eofdata_for_doy(doy).eof1vector))
-        maxdiff_rel_2[idx] = maxdiff_abs_2[idx] / np.mean(np.abs(eofs_ref.eofdata_for_doy(doy).eof2vector))
-    return maxdiff_abs_1, maxdiff_abs_2, maxdiff_rel_1, maxdiff_rel_2
-
-
-def plot_maxdifference_of_eofs_all_doys(recalc_eof: eof.EOFDataForAllDOYs, orig_eof: eof.EOFDataForAllDOYs) -> Figure:
-    """
-    Creates a diagnosis plot showing the correlations for all DOYs of between the original EOFs and newly
-    calculated EOF for both, EOF1 and EOF2
-    :param recalc_eof: The object containing the calculated EOFs
-    :param orig_eof: The object containing the ortiginal EOFs
-    :return: Handle to the figure
-    """
-    doys = eof.doy_list()
-    maxdiff_abs_1, maxdiff_abs_2, maxdiff_rel_1, maxdiff_rel_2 = calc_maxdifference_of_eofs_all_doys(orig_eof, recalc_eof)
-    fig = plt.figure("plot_correlation_with_original_eofs", clear=True, figsize=(6, 4), dpi=150)
-    plt.ylim([0, 1.05])
-    plt.xlabel("DOY")
-    plt.ylabel("Relative Difference")
-    plt.title(" Relative absolute Difference Original-Recalculated EOF")
-    p1, = plt.plot(doys, maxdiff_rel_1, label="EOF1")
-    p2, = plt.plot(doys, maxdiff_rel_2, label="EOF2")
-    plt.legend(handles=(p1, p2))
-    return fig
-
+#FIXME: typing
+#FIXME: Comments
 
 def compute_vector_difference_quantity(ref_vec, vec, percentage=True):
     result = vec - ref_vec
@@ -88,34 +39,146 @@ def compute_vector_difference_quantity(ref_vec, vec, percentage=True):
         result = result / np.mean(np.abs(ref_vec)) *100
     return result
 
-def calc_vector_agreement(ref_vec, vec, percentage=True):
+
+def calc_vector_agreement(ref_vec, vec, percentage=True, do_print=False):
+    if not np.all(ref_vec.size == vec.size):
+        raise AttributeError("Vectors do not have the same lenths.")
     corr = np.corrcoef(ref_vec, vec)[0, 1]
 
-    diff_ts = compute_vector_difference_quantity(ref_vec, vec, percentage=percentage)
-    diff_mean = np.mean(diff_ts)
-    diff_std = np.std(diff_ts)
+    diff_vec = compute_vector_difference_quantity(ref_vec, vec, percentage=percentage)
+    diff_mean = np.mean(diff_vec)
+    diff_std = np.std(diff_vec)
+    diff_abs_percent68 = np.percentile(np.abs(diff_vec), 68.)
+    diff_abs_percent95 = np.percentile(np.abs(diff_vec), 95.)
+    diff_abs_percent99 = np.percentile(np.abs(diff_vec), 99.)
 
-    return corr, diff_mean, diff_std, diff_ts
+    if do_print:
+        print("CorrelationCoefficient: %1.4f" % corr)
+        print("Mean of difference: %1.4f" % diff_mean)
+        print("Stddev. of difference: %1.4f" % diff_std)
+        print("68%% Percentile (abs. value of differences: %1.4f" % diff_abs_percent68)
+        print("95%% Percentile (abs. value of differences: %1.4f" % diff_abs_percent95)
+        print("99%% Percentile (abs. value of differences: %1.4f" % diff_abs_percent99)
+
+    return corr, diff_mean, diff_std, diff_vec, diff_abs_percent68, diff_abs_percent95, diff_abs_percent99
 
 
-def calc_timeseries_agreement(ref_data, ref_time, data, time):
+def calc_comparison_stats_for_eofs_all_doys(eofs_ref: eof.EOFDataForAllDOYs, eofs: eof.EOFDataForAllDOYs, eof_number, exclude_doy366=False, percentage=False, do_print=False) -> typing.Tuple:
+    if eof_number != 1 and eof_number != 2:
+        raise AttributeError("Argument eof_number must be 1 or 2.")
+    doys = eof.doy_list()
+    if exclude_doy366:
+        doys = doys[:-1]
+    corr = np.empty(doys.size)
+    diff_mean= np.empty(doys.size)
+    diff_std= np.empty(doys.size)
+    diff_abs_percent68 = np.empty(doys.size)
+    diff_abs_percent95 = np.empty(doys.size)
+    diff_abs_percent99 = np.empty(doys.size)
+
+    for idx, doy in enumerate(doys):
+        if eof_number == 1:
+            eof_ref = eofs_ref.eof1vector_for_doy(doy)
+            eof_test = eofs.eof1vector_for_doy(doy)
+        elif eof_number == 2:
+            eof_ref = eofs_ref.eof2vector_for_doy(doy)
+            eof_test = eofs.eof2vector_for_doy(doy)
+        corr_single, diff_mean_single, diff_std_single, diff_vec_single, diff_abs_percent68_single, diff_abs_percent95_single, diff_abs_percent99_single = calc_vector_agreement(eof_ref,eof_test,percentage=percentage, do_print=False)
+        corr[idx] = corr_single
+        diff_mean[idx] = diff_mean_single
+        diff_std[idx] = diff_std_single
+        diff_abs_percent68[idx] = diff_abs_percent68_single
+        diff_abs_percent95[idx] = diff_abs_percent95_single
+        diff_abs_percent99[idx] = diff_abs_percent99_single
+
+    if do_print:
+        print("########## Summary of EOF comparison for all DOYs (EOF %i)" % eof_number)
+        print("Worst Correlation (at DOY %i): %1.4f" % (doys[np.argmin(corr)], np.amin(corr)))
+        print("Worst 99%% percentile (at DOY %i): %1.4f" % (doys[np.argmax(diff_abs_percent99)], np.amax(diff_abs_percent99)))
+        print("Worst 68%% percentile (at DOY %i): %1.4f" % (doys[np.argmax(diff_abs_percent68)], np.amax(diff_abs_percent68)))
+
+    return corr, diff_mean, diff_std, diff_abs_percent68, diff_abs_percent95, diff_abs_percent99
+
+
+def plot_comparison_stats_for_eofs_all_doys(recalc_eof: eof.EOFDataForAllDOYs, orig_eof: eof.EOFDataForAllDOYs, exclude_doy366=False, do_print=False) -> Figure:
+    """
+
+    :param recalc_eof: The object containing the calculated EOFs
+    :param orig_eof: The object containing the ortiginal EOFs
+    :return: Handle to the figure
+    """
+
+    doys = eof.doy_list()
+    if exclude_doy366:
+        doys = doys[:-1]
+    corr_1, diff_mean_1, diff_std_1, diff_abs_percent68_1, diff_abs_percent95_1, diff_abs_percent99_1 =  calc_comparison_stats_for_eofs_all_doys(orig_eof, recalc_eof, exclude_doy366=exclude_doy366, eof_number=1, percentage=False, do_print=do_print)
+    corr_2, diff_mean_2, diff_std_2, diff_abs_percent68_2, diff_abs_percent95_2, diff_abs_percent99_2 =  calc_comparison_stats_for_eofs_all_doys(orig_eof, recalc_eof, exclude_doy366=exclude_doy366, eof_number=2, percentage=False, do_print=do_print)
+
+    fig_id = "plot_comparison_stats_for_eofs_all_doys"
+    fig, axs = plt.subplots(4, 1, num=fig_id, clear=True, figsize=(12, 9), dpi=150)
+    plt.subplots_adjust(hspace=0.3)
+
+    fig.suptitle("Comparison of EOFs for all DOYs")
+
+    ax = axs[0]
+    ax.set_title("Correlation")
+    p11, = ax.plot(doys, corr_1, label="EOF1", color="blue")
+    p12, = ax.plot(doys, corr_2, label="EOF2", color="green")
+
+    ax = axs[1]
+    ax.set_title("Mean")
+    p21, = ax.plot(doys, diff_mean_1, label="EOF1", color="blue")
+    p22, = ax.plot(doys, diff_mean_2, label="EOF2", color="green")
+
+    ax = axs[2]
+    ax.set_title("Std Dev")
+    p31, = ax.plot(doys, diff_std_1, label="EOF1", color="blue")
+    p32, = ax.plot(doys, diff_std_2, label="EOF2", color="green")
+
+    ax = axs[3]
+    ax.set_title("Percentiles")
+    p31, = ax.plot(doys, diff_abs_percent99_1, label="EOF1", color="blue")
+    p32, = ax.plot(doys, diff_abs_percent99_2, label="EOF2", color="green")
+    p33, = ax.plot(doys, diff_abs_percent95_1, label="EOF1", color="blue", linestyle="--")
+    p34, = ax.plot(doys, diff_abs_percent95_2, label="EOF2", color="green", linestyle="--")
+    p35, = ax.plot(doys, diff_abs_percent68_1, label="EOF1", color="blue", linestyle=":")
+    p36, = ax.plot(doys, diff_abs_percent68_2, label="EOF2", color="green", linestyle=":")
+
+    return fig
+
+
+def calc_timeseries_agreement(ref_data, ref_time, data, time, exclude_doy366=False, do_print = False):
     if not np.all(ref_time == time):
-        raise AttributeError("Time series do not cover the same lengths.")
-    doys = tools.calc_day_of_year(ref_time)
-    doy_not_366_inds = np.nonzero(doys != 366)
-    doy366_inds = np.nonzero(doys == 366)
+        raise AttributeError("Time series do not cover the same periods.")
+    if exclude_doy366:
+        if do_print:
+            print("###### DOY 366 excluded")
+        doys = tools.calc_day_of_year(ref_time)
+        inds_used = np.nonzero(doys != 366)
+        inds_not_used = np.nonzero(doys == 366)
+        calc_ref_data = ref_data[inds_used]
+        calc_data = data[inds_used]
+    else:
+        if do_print:
+            print("##### Complete time series")
+        calc_ref_data = ref_data
+        calc_data = data
+        inds_used = (np.arange(0, ref_time.size, 1),)
+        inds_not_used = (np.array([], dtype="int64"),)
 
-    corr_complete, diff_mean_complete, diff_std_complete, diff_ts_abs_complete = calc_vector_agreement(ref_data, data, percentage=False)
+    corr, diff_mean, diff_std, diff_ts, diff_abs_percent68, diff_abs_percent95, diff_abs_percent99 = calc_vector_agreement(calc_ref_data, calc_data, percentage=False, do_print=do_print)
 
-    ref_data_not366 = ref_data[doy_not_366_inds]
-    data_not366 = data[doy_not_366_inds]
-    corr_not366, diff_mean_not366, diff_std_not366, diff_ts_abs_not366 = calc_vector_agreement(ref_data_not366, data_not366, percentage=False)
-
-    return doy_not_366_inds, doy366_inds, corr_complete, diff_mean_complete, diff_std_complete, diff_ts_abs_complete, corr_not366, diff_mean_not366, diff_std_not366, diff_ts_abs_not366
+    return inds_used, inds_not_used, corr, diff_mean, diff_std, diff_ts, diff_abs_percent68, diff_abs_percent95, diff_abs_percent99
 
 def plot_timeseries_agreement(ref_data, ref_time, data, time, title = None, do_print=False):
 
-    doy_not_366_inds, doy366_inds, corr_complete, diff_mean_complete, diff_std_complete, diff_ts_abs_complete, corr_not366, diff_mean_not366, diff_std_not366, diff_ts_abs_not366 = calc_timeseries_agreement(ref_data, ref_time, data, time)
+    if do_print:
+        print("##########")
+        if title is not None:
+            print(title)
+    tempa, tempb, corr_complete, diff_mean_complete, diff_std_complete, diff_ts_abs_complete, diff_abs_percent68_complete, diff_abs_percent95_complete, diff_abs_percent99_complete= calc_timeseries_agreement(ref_data, ref_time, data, time, exclude_doy366=False, do_print=do_print)
+    doy_not_366_inds, doy366_inds, corr_not366, diff_mean_not366, diff_std_not366, diff_ts_abs_not366, diff_abs_percent68_not366, diff_abs_percent95_not366, diff_abs_percent99_not366 = calc_timeseries_agreement(ref_data, ref_time, data, time, exclude_doy366=True, do_print=do_print)
+
 
     ref_data_not366 = ref_data[doy_not_366_inds]
     data_not366 = data[doy_not_366_inds]
@@ -150,14 +213,45 @@ def plot_timeseries_agreement(ref_data, ref_time, data, time, title = None, do_p
     mu = diff_mean_not366
     ax.plot(bins2, 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(- (bins2 - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
 
+    return fig
+
+
+def plot_vector_agreement(ref_data, data, title=None, do_print=False):
+
     if do_print:
+        print("##########")
         if title is not None:
             print(title)
-        print("Mean of difference (without DOY 366): %1.2f" % diff_mean_not366)
-        print("Stddev. of difference (without DOY 366): %1.2f" % diff_std_not366)
-        print("68%% Percentile (abs. value of differences; without DOY 366): %1.2f" % np.percentile(np.abs(diff_ts_abs_not366), 68))
-        print(np.percentile(np.abs(diff_ts_abs_not366), 99))
-        print(np.percentile(np.abs(diff_ts_abs_complete), 68))
-        print(np.percentile(np.abs(diff_ts_abs_complete), 99))
+    corr, diff_mean, diff_std, diff_vec, diff_abs_percent68, diff_abs_percent95, diff_abs_percent99 = calc_vector_agreement(ref_data, data, percentage=False, do_print=do_print)
+
+    fig_id = "evaluate_vector_agreement"
+    if title is not None:
+        fig_id = fig_id + "_" + title
+    fig, axs = plt.subplots(2, 2, num=fig_id, clear=True, figsize=(12, 9), dpi=150)
+    plt.subplots_adjust(hspace=0.3)
+    if title is not None:
+        fig.suptitle(title)
+
+    ax = axs[0, 0]
+    ax.set_title("Data")
+    p11, = ax.plot(ref_data, label="Reference")
+    p12, = ax.plot(data, label="Recalculation")
+
+    ax = axs[1, 0]
+    ax.set_title("Difference")
+    p21, = ax.plot( diff_vec, color="red")
+
+    ax = axs[0, 1]
+    ax.set_title("Scatterplot")
+    p31, = ax.plot(ref_data, data, linestyle='None', marker="x", color="blue")
+
+    ax = axs[1, 1]
+    (n2, bins2, patches) = ax.hist(diff_vec, bins=100, density=True)
+
+    sigma = diff_std
+    mu = diff_mean
+    ax.plot(bins2, 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(- (bins2 - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
+
     return fig
+
 
