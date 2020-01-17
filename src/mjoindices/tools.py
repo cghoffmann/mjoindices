fd44d9b5f377efb982261d4360c240293ffa55cb
Christoph Hoffmann <christoph.hoffmann@uni-greifswald.de>
2020-01-17 12:27:09 +0100

    Improved documentation for tools.py

--- src/mjoindices/tools.py
+++ src/mjoindices/tools.py
@@ -25,6 +25,7 @@ import datetime as dt
 import typing
 import numpy as np
 
+
 def calc_day_of_year(date: typing.Union[np.datetime64, np.ndarray]) -> typing.Union[int, np.ndarray]:
     """
     Calculates the days of the year (DOYs) for an individual date or an array of dates.
@@ -59,19 +60,17 @@ def find_doy_ranges_in_dates(dates: np.ndarray, center_doy: int, window_length: 
     :param window_length: the length of the window to both sides  of the center in days.
         The window spans 2*window_length+1 days in total (for exceptions see below).
     :param strict_leap_year_treatmenmt: distinguishes between 2 different methods of constructing the DOY range window.
-        (1) Setting the switch to False will use a pragmatic implementation in which the start and end of the DOY window is
-        directly computed as distance in units of DOYs. (2) Setting the switch to True will transfer the DOYs to
+        1: Setting the switch to False will use a pragmatic implementation in which the start and end of the DOY window
+        is directly computed as distance in units of DOYs. 2: Setting the switch to True will transfer the DOYs to
         actual calender dates and will calculate the start and end of the window also as calender dates using
-        built-in numpy datetime functions.
-
-        In the context of the EOF calculation, the setting has major implications only for the EOSs
-        calculated for DOY 366 and causes only minor differences for the other DOYs. The results for the setting False
-        are closer to the original values, and approximately the same total number of DOYs covered by the window is
-        found for each center DOY including 366. However, the length of the window is not guaranteed to be
-        2*window_length+1, but can also be 2*window_length+2 if the window crosses the ending of a leap year.
-        The setting True is somewhat more stringently implemented. The window length is always 2*window_length+1,
-        however, the number of DOYs covered by a window is reduces by approximately a factor of 4 for center_doy=366,
-        since a window generally found at all only during leap years.
+        built-in numpy datetime functions. In the context of the EOF calculation, the setting has major implications
+        only for the EOFs calculated for DOY 366 and causes only minor differences for the other DOYs. The results for
+        the setting False are closer to the original values, and approximately the same total number of calender days
+        covered by the window is found for each center DOY including 366. However, the length of the window is not
+        guaranteed to be 2*window_length+1, but can also be 2*window_length+2 if the window crosses the ending of a leap
+        year. The setting True is somewhat more stringently implemented. The window length is always 2*window_length+1,
+        however, the number of calender days covered by a window is reduced by approximately a factor of 4 for
+        center_doy=366, since a window is found only during leap years at all.
 
     :return: Tuple with, first, the array of indices and, second, the resulting DOYs for comparison.
     """
@@ -81,16 +80,16 @@ def find_doy_ranges_in_dates(dates: np.ndarray, center_doy: int, window_length: 
         center_inds = np.nonzero(doys == center_doy)
 
         # switch from DOYs to real dates to use built-in leap year functionality
-        startdates = dates[center_inds] - np.timedelta64(window_length,'D')
+        startdates = dates[center_inds] - np.timedelta64(window_length, 'D')
         too_early_inds = np.nonzero(startdates < dates[0])
         startdates[too_early_inds] = dates[0]
 
-        enddates=dates[center_inds] + np.timedelta64(window_length,'D')
+        enddates = dates[center_inds] + np.timedelta64(window_length, 'D')
         too_late_inds = np.nonzero(enddates > dates[-1])
         enddates[too_late_inds] = dates[-1]
 
-        resulting_idxlist = np.array([],dtype="int")
-        for ind,startdate in enumerate(startdates):
+        resulting_idxlist = np.array([], dtype="int")
+        for ind, startdate in enumerate(startdates):
             one_window_indices = np.nonzero((dates >= startdates[ind]) & (dates <= enddates[ind]))[0]
             resulting_idxlist = np.concatenate((resulting_idxlist, one_window_indices))
     else:
